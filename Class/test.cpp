40ecba8c1a54c5234926680212832f218c1550b9
orange-cors <mango.cors@gmail.com>
2025-11-18 22:04:41 +0700

    Update

---
+++ Class/test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <math.h>
+using namespace std;
+int main(){
+    int n;
+    int i = 2;
+    cin >> n;
+    if ( n < 1 || n > 1000000);
+    else
+    while (n != -1) { // -1 là giá trị lính canh
+    if ( n < 2);
+    else{
+    while ( i <= sqrt(n) ){
+    if ( n % i == 0 ){
+    cout << n << " Khong phai so nguyen to" << endl;
+    break;
+    }
+    else{
+    cout << n << " La so nguyen to" << endl;
+    break;
+        
+    ++i;
+
+    
+    }
+    ++i;
+    }
+}
+}
+return 0;
+}
